6b9c641ac56aa6658f690d340892813b327ab7f2
Erich Birngruber <erich.birngruber@gmi.oeaw.ac.at>
2019-08-04 13:49:50 +0200

    initial setup for Jenkins image build

--- init.groovy.d/adminUser.groovy
+++ init.groovy.d/adminUser.groovy
@@ -2,31 +2,32 @@
  * Create an admin user. 
  * FIXME this might be conflicting with what is coming from config as code setup
  */
-import jenkins.model.*
-import hudson.security.*
-
-println "--> creating admin user"
-
-def jenkins = Jenkins.getInstance()
-
-def adminUsername = System.getenv("ADMIN_USERNAME")
-def adminPassword = System.getenv("ADMIN_PASSWORD")
-assert adminPassword != null : "No ADMIN_USERNAME env var provided, but required"
-assert adminPassword != null : "No ADMIN_PASSWORD env var provided, but required"
-
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount(adminUsername, adminPassword)
-jenkins.setSecurityRealm(hudsonRealm)
-
-// ProjectMatrixAuthorizationStrategy
-def strategy = jenkins.getAuthorizationStrategy()
-if ((strategy instanceof ProjectMatrixAuthorizationStrategy) == false) {
-  println "adminUser script: setting ProjectMatrixAuthorizationStrategy "
-  strategy = new ProjectMatrixAuthorizationStrategy()
-  jenkins.setAuthorizationStrategy(strategy)
-}
-// admin we just created should have admin privs
-strategy.add(Jenkins.ADMINISTER, adminUsername)
-
-jenkins.save()
+// import jenkins.model.*
+// import hudson.security.*
+// 
+// println "--> creating admin user"
+// 
+// def jenkins = Jenkins.getInstance()
+// 
+// def adminUsername = System.getenv("ADMIN_USERNAME")
+// def adminPassword = System.getenv("ADMIN_PASSWORD")
+// assert adminPassword != null : "No ADMIN_USERNAME env var provided, but required"
+// assert adminPassword != null : "No ADMIN_PASSWORD env var provided, but required"
+// 
+// def securityRealm = jenkins.getSecurityRealm()
+// // new HudsonPrivateSecurityRealm(false)
+// securityRealm.createAccount(adminUsername, adminPassword)
+// //jenkins.setSecurityRealm(hudsonRealm)
+// 
+// // ProjectMatrixAuthorizationStrategy
+// def strategy = jenkins.getAuthorizationStrategy()
+// if ((strategy instanceof ProjectMatrixAuthorizationStrategy) == false) {
+//   println "adminUser script: setting ProjectMatrixAuthorizationStrategy "
+//   strategy = new ProjectMatrixAuthorizationStrategy()
+//   jenkins.setAuthorizationStrategy(strategy)
+// }
+// // admin we just created should have admin privs
+// strategy.add(Jenkins.ADMINISTER, adminUsername)
+// 
+// jenkins.save()
 
